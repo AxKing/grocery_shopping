e0a7b40adbac9118f374c41ae53faf63d7a57cef
Tony Medina <anthony.o.medina@gmail.com>
2023-11-25 21:18:07 -0600

    Added new functionality to get things like TP and milk

--- grocery_shopping.py
+++ grocery_shopping.py
@@ -9,10 +9,12 @@ from pick import pick
 food_items_data = json.load(open("configs/food_items.json"))
 recipes_data = json.load(open("configs/recipes.json"))
 weekly_items_data = json.load(open("configs/weekly_items.json"))
+seldom_items_data = json.load(open("configs/seldom_items.json"))
 
 food_items = {}
 recipes = {}
 weekly_items = []
+seldom_items = []
 
 # Loading in all of the food items from the json to the food class
 for name, attributes in food_items_data.items():
@@ -26,16 +28,22 @@ for name, attributes in recipes_data.items():
         items_in_recipe.append((food_items[item], quantity))
     recipes[name] = Recipe(name, items_in_recipe)
 
+
 # converting weekly items to food items and loading them into the weekly list. 
 for weekly_item, quantity in weekly_items_data:
     weekly_items.append((food_items[weekly_item], quantity))
+
+# converting seldom items to food item objects
+for seldom_item, quantity in seldom_items_data:
+    seldom_items.append((food_items[seldom_item], quantity))
+
 
 # Picking the recipes that we need to add for the week.
 weekly_recipes = []
 last_selected = None
 options = list(recipes.keys())
 options.append("--Exit")
-print(options)
+# print(options)
 finished_adding_recipes = False
 while not finished_adding_recipes:
     if last_selected == None:
@@ -50,16 +58,38 @@ while not finished_adding_recipes:
         last_selected = selected
 # print(weekly_recipes)
 
+#
+"""
+1. list the seldom items
+2. add the items to the "Weekly Items List"
+"""
+last_selected = None
+seldom_items_data
+options = [x[0].name for x in seldom_items]
+options.append("--Exit")
+# print(options)
+finished_adding_seldom_items = False
+while not finished_adding_seldom_items:
+    if last_selected == None:
+        title = 'Do you need any of these for this trip? (ENTER to select): '
+    else:
+        title = 'I have added ' + last_selected + ' to the cart.'
+    selected, index = pick(options, title, multiselect=False)
+    
+    if selected == "--Exit":
+        finished_adding_seldom_items = True
+    else:
+        weekly_items.append(seldom_items[index])
+        last_selected = selected
+  
 
 # After we have picked our recipes for the week, we compile the shopping list for those items.
 grocery_list = Shopping_list(weekly_recipes)
 grocery_list.compile_recipe_list()
-# Add the weekly
-# Taking weekly items json and converting it to a weekly_items list of food/quantity touples
 grocery_list.add_weekly_items(weekly_items)
-grocery_list.list_foods_by_department()
-# pprint(grocery_list.final_list)
-grocery_list.list_the_list()
 
-# final_list = add_weekly_items(grocery_list)
-# print(final_list)
+
+# This is where I will call Adding the non-weekly items to the list
+
+grocery_list.list_foods_by_department()
+grocery_list.list_the_list()
